8c2e1aa2e06888e1e4d83940c7d5948682dbbd91
Khrapal Maksym <khrapal_m_g@outlook.com>
2026-03-04 22:43:11 +0200

    Add project files.

---
+++ src/Interception.UI/Infrastructure/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿//-----------------------------------------------------------------------------
+// All rights by agreement of the developer. Author data on GitHub Khrapal M.G.
+//-----------------------------------------------------------------------------
+
+using Microsoft.EntityFrameworkCore;
+
+namespace Interception.UI.Infrastructure;
+
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+{
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // Postgres розширення для темпоральних обмежень
+        if (Database.IsNpgsql())
+        {
+            modelBuilder.HasPostgresExtension("btree_gist");
+        }
+
+        // Застосувати всі конфігурації з поточної збірки
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+    }
+}
---
+++ src/Interception.UI/Program.cs
@@ -0,0 +1,50 @@
+//-----------------------------------------------------------------------------
+// All rights by agreement of the developer. Author data on GitHub Khrapal M.G.
+//-----------------------------------------------------------------------------
+
+using Interception.UI.Components;
+using Interception.UI.Infrastructure;
+using Microsoft.AspNetCore.DataProtection;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Razor + Blazor Server (Interactive)
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents(opt => { opt.DetailedErrors = true; });
+
+builder.Services.AddDbContextFactory<AppDbContext>(options =>
+{
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")); // або ваш провайдер
+});
+
+// DataProtection keys must survive restarts and be shared across instances
+builder.Services.AddDataProtection()
+    .PersistKeysToFileSystem(new DirectoryInfo("/data-protection-keys"));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
+app.UseHttpsRedirection();
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Use(async (ctx, next) =>
+{
+    ctx.Response.Headers.ContentSecurityPolicy =
+        "frame-ancestors 'none'";
+    await next();
+});
+
+app.Run();
