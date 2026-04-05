de0525105b178cd990372a850b8f472d7daf3cf4
Khrapal Maksym <72307872+Khrapal-Max@users.noreply.github.com>
2026-04-05 19:21:36 +0300

    Add LinkMap UI test for bridge primary action display

--- tests/Interception.Tests/Components/Analytics/AnalyticsPagesTests.cs
+++ tests/Interception.Tests/Components/Analytics/AnalyticsPagesTests.cs
@@ -84,4 +84,68 @@ public sealed class LinkMapPageTests : BunitContext
 
         _linkMapService.Received(1).BuildAsync(Arg.Any<DateTime?>(), Arg.Any<DateTime?>(), Arg.Any<CancellationToken>());
     }
+
+    [Fact]
+    public void Render_MapWithBridge_ShowsBridgePrimaryActionInFocusedPanel()
+    {
+        var groupBKey = "GROUP-B";
+        var groupAKey = "GROUP-A";
+        var now = new DateTime(2026, 03, 28, 12, 0, 0, DateTimeKind.Utc);
+
+        var groupB = new LinkMapGroupDto(
+            groupBKey,
+            "Батальйон Б",
+            ["145.1000"],
+            "Б-ЦЕНТР",
+            "координатор",
+            ["Б-ЦЕНТР", "Б-1", "Б-2"],
+            [
+                new LinkMapMemberDto("Б-ЦЕНТР", "координатор", 3, 2, 4, now, 1, false, true),
+                new LinkMapMemberDto("Б-1", "оператор", 1, 1, 1, now, 1, false, false),
+                new LinkMapMemberDto("Б-2", "оператор", 1, 1, 1, now, 1, false, false)
+            ],
+            3,
+            6,
+            2,
+            "нанесення ураження",
+            ["нанесення ураження"],
+            []);
+
+        var groupA = new LinkMapGroupDto(
+            groupAKey,
+            "Батальйон А",
+            ["402.0000"],
+            "А-ЦЕНТР",
+            "координатор",
+            ["А-ЦЕНТР", "А-1", "А-2"],
+            [
+                new LinkMapMemberDto("А-ЦЕНТР", "координатор", 3, 2, 4, now, 1, false, true),
+                new LinkMapMemberDto("А-1", "оператор", 1, 1, 1, now, 1, false, false),
+                new LinkMapMemberDto("А-2", "оператор", 1, 1, 1, now, 1, false, false)
+            ],
+            3,
+            6,
+            2,
+            "координація переміщення піхоти",
+            ["координація переміщення піхоти"],
+            [
+                new LinkMapBridgeDto(
+                    groupBKey,
+                    "Батальйон Б",
+                    "Б-ЦЕНТР",
+                    "401.2000",
+                    2,
+                    "розвід інформація",
+                    ["розвід інформація"])
+            ]);
+
+        _linkMapService.BuildAsync(Arg.Any<DateTime?>(), Arg.Any<DateTime?>(), Arg.Any<CancellationToken>())
+            .Returns(new LinkMapDto([groupA, groupB]));
+
+        var cut = Render<LinkMapPage>();
+
+        cut.Markup.Should().Contain("розвід інформація");
+        cut.Markup.Should().Contain("А-ЦЕНТР");
+        cut.Markup.Should().Contain("Б-ЦЕНТР");
+    }
 }
